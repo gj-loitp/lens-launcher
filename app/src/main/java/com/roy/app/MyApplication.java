979dd0e6a3ce21a187bf7189def8fb1864b3a999
loitp <www.muathu@gmail.com>
2023-03-18 22:35:31 +0700

    update

---
+++ app/src/main/java/com/roy/app/MyApplication.java
@@ -0,0 +1,48 @@
+package com.roy.app;
+
+import com.orm.SugarApp;
+import com.roy.background.EditedObservable;
+import com.roy.background.SortAppsTask;
+import com.roy.background.UpdateAppsTask;
+import com.roy.background.UpdatedObservable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by nicholasrout on 2016/06/12.
+ */
+public class MyApplication extends SugarApp implements Observer {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        UpdatedObservable.getInstance().addObserver(this);
+        EditedObservable.getInstance().addObserver(this);
+        updateApps();
+    }
+
+    @Override
+    public void update(Observable observable, Object data) {
+        if (observable instanceof UpdatedObservable) {
+            updateApps();
+        } else if (observable instanceof EditedObservable) {
+            editApps();
+        }
+    }
+
+    private void updateApps() {
+        new UpdateAppsTask(
+                getPackageManager(),
+                getApplicationContext(),
+                this)
+                .execute();
+    }
+
+    private void editApps() {
+        new SortAppsTask(
+                getApplicationContext(),
+                this)
+                .execute();
+    }
+}
