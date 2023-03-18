0229950ac84d06e3912d8ca2518eca5a615235fe
loitp <www.muathu@gmail.com>
2023-03-18 22:18:25 +0700

    update

--- app/src/main/java/com/roy/model/Grid.kt
+++ app/src/main/java/com/roy/model/Grid.kt
@@ -1,76 +1,21 @@
-package com.roy.model;
+package com.roy.model
 
-/**
- * Created by nickrout on 2016/04/02.
- */
-public class Grid {
+class Grid {
+    var itemCount = 0
+    var itemCountHorizontal = 0
+    var itemCountVertical = 0
+    var itemSize = 0f
+    var spacingHorizontal = 0f
+    var spacingVertical = 0f
 
-    private int mItemCount;
-    private int mItemCountHorizontal;
-    private int mItemCountVertical;
-    private float mItemSize;
-    private float mSpacingHorizontal;
-    private float mSpacingVertical;
-
-    public Grid() {}
-
-    public int getItemCount() {
-        return mItemCount;
-    }
-
-    public void setItemCount(int itemCount) {
-        mItemCount = itemCount;
-    }
-
-    public int getItemCountHorizontal() {
-        return mItemCountHorizontal;
-    }
-
-    public void setItemCountHorizontal(int itemCountHorizontal) {
-        mItemCountHorizontal = itemCountHorizontal;
-    }
-
-    public int getItemCountVertical() {
-        return mItemCountVertical;
-    }
-
-    public void setItemCountVertical(int itemCountVertical) {
-        mItemCountVertical = itemCountVertical;
-    }
-
-    public float getItemSize() {
-        return mItemSize;
-    }
-
-    public void setItemSize(float itemSize) {
-        mItemSize = itemSize;
-    }
-
-    public float getSpacingHorizontal() {
-        return mSpacingHorizontal;
-    }
-
-    public void setSpacingHorizontal(float spacingHorizontal) {
-        mSpacingHorizontal = spacingHorizontal;
-    }
-
-    public float getSpacingVertical() {
-        return mSpacingVertical;
-    }
-
-    public void setSpacingVertical(float spacingVertical) {
-        mSpacingVertical = spacingVertical;
-    }
-
-    @Override
-    public String toString() {
+    override fun toString(): String {
         return "Grid{" +
-                "mItemCount=" + mItemCount +
-                ", mItemCountHorizontal=" + mItemCountHorizontal +
-                ", mItemCountVertical=" + mItemCountVertical +
-                ", mItemSize=" + mItemSize +
-                ", mSpacingHorizontal=" + mSpacingHorizontal +
-                ", mSpacingVertical=" + mSpacingVertical +
-                '}';
+                "mItemCount=" + itemCount +
+                ", mItemCountHorizontal=" + itemCountHorizontal +
+                ", mItemCountVertical=" + itemCountVertical +
+                ", mItemSize=" + itemSize +
+                ", mSpacingHorizontal=" + spacingHorizontal +
+                ", mSpacingVertical=" + spacingVertical +
+                '}'
     }
 }
