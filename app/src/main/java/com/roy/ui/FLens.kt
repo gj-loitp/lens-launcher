b5ba8e6e7ac2ba22c7490108d4259b015851a976
loitp <www.muathu@gmail.com>
2023-03-19 22:54:29 +0700

    update

--- app/src/main/java/com/roy/ui/FLens.kt
+++ app/src/main/java/com/roy/ui/FLens.kt
@@ -1,195 +1,229 @@
-package com.roy.ui;
+package com.roy.ui
 
-import android.content.Context;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatSeekBar
+import androidx.fragment.app.Fragment
+import com.roy.R
+import com.roy.itf.LensInterface
+import com.roy.util.UtilSettings
+import com.roy.views.LensView
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatSeekBar;
-import androidx.fragment.app.Fragment;
-
-import com.roy.R;
-import com.roy.itf.LensInterface;
-import com.roy.util.UtilSettings;
-import com.roy.views.LensView;
-
-public class FLens extends Fragment implements LensInterface {
-
-    LensView lensViewsSettings;
-    AppCompatSeekBar sbMinIconSize;
-    TextView tvValueMinIconSize;
-    AppCompatSeekBar sbDistortionFactor;
-    TextView tvValueDistortionFactor;
-    AppCompatSeekBar sbScaleFactor;
-    TextView tvValueScaleFactor;
-    AppCompatSeekBar sbAnimationTime;
-    TextView tvValueAnimationTime;
-
-    private UtilSettings utilSettings;
-
-    public FLens() {
-    }
-
-    public static FLens newInstance() {
-        return new FLens();
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.f_lens, container, false);
-        utilSettings = new UtilSettings(getActivity());
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        setupViews(view);
-        assignValues();
-    }
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (getActivity() != null && getActivity() instanceof ASettings) {
-            ((ASettings) getActivity()).setLensInterface(this);
+class FLens : Fragment(), LensInterface {
+    companion object {
+        fun newInstance(): FLens {
+            return FLens()
         }
     }
 
-    private void setupViews(View view) {
-        lensViewsSettings = view.findViewById(R.id.lensViewsSettings);
-        sbMinIconSize = view.findViewById(R.id.sbMinIconSize);
-        tvValueMinIconSize = view.findViewById(R.id.tvValueMinIconSize);
-        sbDistortionFactor = view.findViewById(R.id.sbDistortionFactor);
-        tvValueDistortionFactor = view.findViewById(R.id.tvValueDistortionFactor);
-        sbScaleFactor = view.findViewById(R.id.sbScaleFactor);
-        tvValueScaleFactor = view.findViewById(R.id.tvValueScaleFactor);
-        sbAnimationTime = view.findViewById(R.id.sbAnimationTime);
-        tvValueAnimationTime = view.findViewById(R.id.tvValueAnimationTime);
+    @JvmField
+    var lensViewsSettings: LensView? = null
 
-        view.findViewById(R.id.sbMinIconSizeParent).setOnClickListener(v -> {
-            //do nothing
-        });
-        view.findViewById(R.id.rlSbDistortionFactorParent).setOnClickListener(v -> {
-            //do nothing
-        });
-        view.findViewById(R.id.rlSbScaleFactorParent).setOnClickListener(v -> {
-            //do nothing
-        });
-        view.findViewById(R.id.rlSbAnimationTimeParent).setOnClickListener(v -> {
-            //do nothing
-        });
+    @JvmField
+    var sbMinIconSize: AppCompatSeekBar? = null
 
-        lensViewsSettings.setDrawType(LensView.DrawType.CIRCLES);
-        sbMinIconSize.setMax(UtilSettings.MAX_ICON_SIZE);
-        sbMinIconSize.setOnSeekBarChangeListener(new AppCompatSeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int appropriateProgress = progress + (int) UtilSettings.MIN_ICON_SIZE;
-                String minIconSize = appropriateProgress + "dp";
-                tvValueMinIconSize.setText(minIconSize);
-                utilSettings.save(UtilSettings.KEY_ICON_SIZE, (float) appropriateProgress);
-                lensViewsSettings.invalidate();
-            }
+    @JvmField
+    var tvValueMinIconSize: TextView? = null
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
+    @JvmField
+    var sbDistortionFactor: AppCompatSeekBar? = null
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-        sbDistortionFactor.setMax(UtilSettings.MAX_DISTORTION_FACTOR);
-        sbDistortionFactor.setOnSeekBarChangeListener(new AppCompatSeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float appropriateProgress = (float) progress / 2.0f + UtilSettings.MIN_DISTORTION_FACTOR;
-                String distortionFactor = appropriateProgress + "";
-                tvValueDistortionFactor.setText(distortionFactor);
-                utilSettings.save(UtilSettings.KEY_DISTORTION_FACTOR, appropriateProgress);
-                lensViewsSettings.invalidate();
-            }
+    @JvmField
+    var tvValueDistortionFactor: TextView? = null
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
+    @JvmField
+    var sbScaleFactor: AppCompatSeekBar? = null
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-        sbScaleFactor.setMax(UtilSettings.MAX_SCALE_FACTOR);
-        sbScaleFactor.setOnSeekBarChangeListener(new AppCompatSeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                float appropriateProgress = (float) progress / 5.0f + UtilSettings.MIN_SCALE_FACTOR;
-                String scaleFactor = appropriateProgress + "";
-                tvValueScaleFactor.setText(scaleFactor);
-                utilSettings.save(UtilSettings.KEY_SCALE_FACTOR, appropriateProgress);
-                lensViewsSettings.invalidate();
-            }
+    @JvmField
+    var tvValueScaleFactor: TextView? = null
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
+    @JvmField
+    var sbAnimationTime: AppCompatSeekBar? = null
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
-        sbAnimationTime.setMax(UtilSettings.MAX_ANIMATION_TIME);
-        sbAnimationTime.setOnSeekBarChangeListener(new AppCompatSeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                long appropriateProgress = (long) progress / 2 + UtilSettings.MIN_ANIMATION_TIME;
-                String animationTime = appropriateProgress + "ms";
-                tvValueAnimationTime.setText(animationTime);
-                utilSettings.save(UtilSettings.KEY_ANIMATION_TIME, appropriateProgress);
-            }
+    @JvmField
+    var tvValueAnimationTime: TextView? = null
+    private var utilSettings: UtilSettings? = null
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.f_lens, container, false)
+        utilSettings = UtilSettings(activity)
+        return view
     }
 
-    private void assignValues() {
-        sbMinIconSize.setProgress((int) utilSettings.getFloat(UtilSettings.KEY_ICON_SIZE) - (int) UtilSettings.MIN_ICON_SIZE);
-        String minIconSize = (int) utilSettings.getFloat(UtilSettings.KEY_ICON_SIZE) + "dp";
-        tvValueMinIconSize.setText(minIconSize);
-        sbDistortionFactor.setProgress((int) (2.0f * (utilSettings.getFloat(UtilSettings.KEY_DISTORTION_FACTOR) - UtilSettings.MIN_DISTORTION_FACTOR)));
-        String distortionFactor = utilSettings.getFloat(UtilSettings.KEY_DISTORTION_FACTOR) + "";
-        tvValueDistortionFactor.setText(distortionFactor);
-        sbScaleFactor.setProgress((int) (5.0f * (utilSettings.getFloat(UtilSettings.KEY_SCALE_FACTOR) - UtilSettings.MIN_SCALE_FACTOR)));
-        String scaleFactor = utilSettings.getFloat(UtilSettings.KEY_SCALE_FACTOR) + "";
-        tvValueScaleFactor.setText(scaleFactor);
-        sbAnimationTime.setProgress((int) (2 * (utilSettings.getLong(UtilSettings.KEY_ANIMATION_TIME) - UtilSettings.MIN_ANIMATION_TIME)));
-        String animationTime = utilSettings.getLong(UtilSettings.KEY_ANIMATION_TIME) + "ms";
-        tvValueAnimationTime.setText(animationTime);
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViews(view)
+        assignValues()
     }
 
-    @Override
-    public void onDefaultsReset() {
-        resetToDefault();
-        assignValues();
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (activity != null && activity is ASettings) {
+            (activity as ASettings?)?.setLensInterface(this)
+        }
     }
 
-    private void resetToDefault() {
-        utilSettings.save(UtilSettings.KEY_ICON_SIZE, UtilSettings.DEFAULT_ICON_SIZE);
-        utilSettings.save(UtilSettings.KEY_DISTORTION_FACTOR, UtilSettings.DEFAULT_DISTORTION_FACTOR);
-        utilSettings.save(UtilSettings.KEY_SCALE_FACTOR, UtilSettings.DEFAULT_SCALE_FACTOR);
-        utilSettings.save(UtilSettings.KEY_ANIMATION_TIME, UtilSettings.DEFAULT_ANIMATION_TIME);
+    private fun setupViews(view: View) {
+        lensViewsSettings = view.findViewById(R.id.lensViewsSettings)
+        sbMinIconSize = view.findViewById(R.id.sbMinIconSize)
+        tvValueMinIconSize = view.findViewById(R.id.tvValueMinIconSize)
+        sbDistortionFactor = view.findViewById(R.id.sbDistortionFactor)
+        tvValueDistortionFactor = view.findViewById(R.id.tvValueDistortionFactor)
+        sbScaleFactor = view.findViewById(R.id.sbScaleFactor)
+        tvValueScaleFactor = view.findViewById(R.id.tvValueScaleFactor)
+        sbAnimationTime = view.findViewById(R.id.sbAnimationTime)
+        tvValueAnimationTime = view.findViewById(R.id.tvValueAnimationTime)
+
+        view.findViewById<View>(R.id.sbMinIconSizeParent).setOnClickListener { }
+        view.findViewById<View>(R.id.rlSbDistortionFactorParent).setOnClickListener { }
+        view.findViewById<View>(R.id.rlSbScaleFactorParent).setOnClickListener { }
+        view.findViewById<View>(R.id.rlSbAnimationTimeParent).setOnClickListener { }
+
+        lensViewsSettings?.setDrawType(LensView.DrawType.CIRCLES)
+
+        sbMinIconSize?.let { sb ->
+            sb.max = UtilSettings.MAX_ICON_SIZE
+            sb.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    val appropriateProgress = progress + UtilSettings.MIN_ICON_SIZE.toInt()
+                    val minIconSize = appropriateProgress.toString() + "dp"
+                    tvValueMinIconSize?.text = minIconSize
+                    utilSettings?.save(UtilSettings.KEY_ICON_SIZE, appropriateProgress.toFloat())
+                    lensViewsSettings?.invalidate()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            })
+        }
+
+        sbDistortionFactor?.let { sb ->
+            sb.max = UtilSettings.MAX_DISTORTION_FACTOR
+            sb.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    val appropriateProgress =
+                        progress.toFloat() / 2.0f + UtilSettings.MIN_DISTORTION_FACTOR
+                    val distortionFactor = appropriateProgress.toString() + ""
+                    tvValueDistortionFactor?.text = distortionFactor
+                    utilSettings?.save(UtilSettings.KEY_DISTORTION_FACTOR, appropriateProgress)
+                    lensViewsSettings?.invalidate()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            })
+        }
+
+        sbScaleFactor?.let { sb ->
+            sb.max = UtilSettings.MAX_SCALE_FACTOR
+            sb.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    val appropriateProgress =
+                        progress.toFloat() / 5.0f + UtilSettings.MIN_SCALE_FACTOR
+                    val scaleFactor = appropriateProgress.toString() + ""
+                    tvValueScaleFactor?.text = scaleFactor
+                    utilSettings?.save(UtilSettings.KEY_SCALE_FACTOR, appropriateProgress)
+                    lensViewsSettings?.invalidate()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            })
+        }
+
+        sbAnimationTime?.let { sb ->
+            sb.max = UtilSettings.MAX_ANIMATION_TIME
+            sb.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    val appropriateProgress =
+                        progress.toLong() / 2 + UtilSettings.MIN_ANIMATION_TIME
+                    val animationTime = appropriateProgress.toString() + "ms"
+                    tvValueAnimationTime?.text = animationTime
+                    utilSettings?.save(UtilSettings.KEY_ANIMATION_TIME, appropriateProgress)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            })
+        }
+    }
+
+    private fun assignValues() {
+        utilSettings?.let { us ->
+            sbMinIconSize?.progress =
+                us.getFloat(UtilSettings.KEY_ICON_SIZE).toInt() - UtilSettings.MIN_ICON_SIZE.toInt()
+            val minIconSize =
+                us.getFloat(UtilSettings.KEY_ICON_SIZE).toInt().toString() + "dp"
+            tvValueMinIconSize?.text = minIconSize
+            sbDistortionFactor?.progress =
+                (2.0f * (us.getFloat(UtilSettings.KEY_DISTORTION_FACTOR) - UtilSettings.MIN_DISTORTION_FACTOR)).toInt()
+
+            val distortionFactor =
+                us.getFloat(UtilSettings.KEY_DISTORTION_FACTOR).toString() + ""
+            tvValueDistortionFactor?.text = distortionFactor
+            sbScaleFactor?.progress =
+                (5.0f * (us.getFloat(UtilSettings.KEY_SCALE_FACTOR) - UtilSettings.MIN_SCALE_FACTOR)).toInt()
+
+            val scaleFactor = us.getFloat(UtilSettings.KEY_SCALE_FACTOR).toString() + ""
+            tvValueScaleFactor?.text = scaleFactor
+            sbAnimationTime?.progress =
+                (2 * (us.getLong(UtilSettings.KEY_ANIMATION_TIME) - UtilSettings.MIN_ANIMATION_TIME)).toInt()
+            val animationTime =
+                us.getLong(UtilSettings.KEY_ANIMATION_TIME).toString() + "ms"
+            tvValueAnimationTime?.text = animationTime
+        }
+    }
+
+    override fun onDefaultsReset() {
+        resetToDefault()
+        assignValues()
+    }
+
+    private fun resetToDefault() {
+        utilSettings?.let { us ->
+            us.save(
+                /* name = */ UtilSettings.KEY_ICON_SIZE,
+                /* value = */ UtilSettings.DEFAULT_ICON_SIZE
+            )
+            us.save(
+                /* name = */ UtilSettings.KEY_DISTORTION_FACTOR,
+                /* value = */ UtilSettings.DEFAULT_DISTORTION_FACTOR
+            )
+            us.save(
+                /* name = */ UtilSettings.KEY_SCALE_FACTOR,
+                /* value = */ UtilSettings.DEFAULT_SCALE_FACTOR
+            )
+            us.save(
+                /* name = */ UtilSettings.KEY_ANIMATION_TIME,
+                /* value = */ UtilSettings.DEFAULT_ANIMATION_TIME
+            )
+        }
     }
 }
