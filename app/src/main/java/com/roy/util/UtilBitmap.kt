bca3056e8ad47279244bd74b66ca31eec334c78d
loitp <www.muathu@gmail.com>
2023-03-20 11:59:05 +0700

    update

--- app/src/main/java/com/roy/util/UtilBitmap.kt
+++ app/src/main/java/com/roy/util/UtilBitmap.kt
@@ -1,98 +1,138 @@
-package com.roy.util;
+package com.roy.util
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.content.res.Resources.NotFoundException
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 
-public class UtilBitmap {
-
+object UtilBitmap {
     // Old method - does not allow for adding options i.e. lower quality
     // Source: http://stackoverflow.com/questions/3035692/how-to-convert-a-drawable-to-a-bitmap
-    public static Bitmap drawableToBitmap(Drawable drawable) {
-        Bitmap bitmap;
-
-        if (drawable instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-            bitmapDrawable.setAntiAlias(true);
-            bitmapDrawable.setDither(true);
-            bitmapDrawable.setTargetDensity(Integer.MAX_VALUE);
-            if (bitmapDrawable.getBitmap() != null) {
-                return bitmapDrawable.getBitmap();
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            drawable.setAntiAlias(true)
+            drawable.setDither(true)
+            drawable.setTargetDensity(Int.MAX_VALUE)
+            if (drawable.bitmap != null) {
+                return drawable.bitmap
             }
         }
-
-        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        val bitmap: Bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
+            Bitmap.createBitmap(
+                /* width = */ 1,
+                /* height = */ 1,
+                /* config = */ Bitmap.Config.ARGB_8888
+            ) // Single color bitmap will be created of 1x1 pixel
         } else {
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Bitmap.createBitmap(
+                /* width = */ drawable.intrinsicWidth,
+                /* height = */ drawable.intrinsicHeight,
+                /* config = */ Bitmap.Config.ARGB_8888
+            )
         }
-
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(
+            /* left = */ 0,
+            /* top = */ 0,
+            /* right = */ canvas.width,
+            /* bottom = */ canvas.height
+        )
+        drawable.draw(canvas)
+        return bitmap
     }
 
     // Get bitmap from resource id, with quality options
-    public static Bitmap resIdToBitmap(Resources res, int resId) {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inScaled = false;
-        options.inDither = false;
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        options.inPreferQualityOverSpeed = true;
-        try {
-            return BitmapFactory.decodeResource(res, resId, options);
-        } catch (OutOfMemoryError e1) {
-            e1.printStackTrace();
-            return null;
+    private fun resIdToBitmap(
+        res: Resources?,
+        resId: Int
+    ): Bitmap? {
+        val options = BitmapFactory.Options()
+        options.inScaled = false
+        options.inDither = false
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888
+        options.inPreferQualityOverSpeed = true
+        return try {
+            BitmapFactory.decodeResource(
+                /* res = */ res,
+                /* id = */ resId,
+                /* opts = */ options
+            )
+        } catch (e1: OutOfMemoryError) {
+            e1.printStackTrace()
+            null
         }
     }
 
     // Get res id from app package name
-    public static int packageNameToResId(PackageManager packageManager, String packageName) {
-        try {
-            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-            return applicationInfo.icon;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-            return -1;
+    fun packageNameToResId(
+        packageManager: PackageManager,
+        packageName: String
+    ): Int {
+        return try {
+            val applicationInfo = packageManager.getApplicationInfo(
+                /* p0 = */ packageName,
+                /* p1 = */ PackageManager.GET_META_DATA
+            )
+            applicationInfo.icon
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            -1
         }
     }
 
     // Get bitmap from app package name
-    public static Bitmap packageNameToBitmap(PackageManager packageManager, String packageName) {
-        try {
-            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-            Resources resources = packageManager.getResourcesForApplication(applicationInfo);
-            int appIconResId = applicationInfo.icon;
-            return resIdToBitmap(resources, appIconResId);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-            return null;
+    fun packageNameToBitmap(
+        packageManager: PackageManager,
+        packageName: String
+    ): Bitmap? {
+        return try {
+            val applicationInfo =
+                packageManager.getApplicationInfo(
+                    /* p0 = */ packageName,
+                    /* p1 = */ PackageManager.GET_META_DATA
+                )
+            val resources = packageManager.getResourcesForApplication(applicationInfo)
+            val appIconResId = applicationInfo.icon
+            resIdToBitmap(resources, appIconResId)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            null
         }
     }
 
     // Get bitmap from app package name (with res id)
-    public static Bitmap packageNameToBitmap(PackageManager packageManager, String packageName, int resId) {
-        try {
-            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-            Resources resources = packageManager.getResourcesForApplication(applicationInfo);
-            Bitmap bitmap = resIdToBitmap(resources, resId);
+    fun packageNameToBitmap(
+        packageManager: PackageManager,
+        packageName: String,
+        resId: Int
+    ): Bitmap? {
+        return try {
+            val applicationInfo = packageManager.getApplicationInfo(
+                /* p0 = */ packageName,
+                /* p1 = */ PackageManager.GET_META_DATA
+            )
+            val resources = packageManager.getResourcesForApplication(applicationInfo)
+            var bitmap = resIdToBitmap(
+                res = resources,
+                resId = resId
+            )
             if (bitmap == null) {
-                Drawable drawable = packageManager.getApplicationIcon(packageName);
-                if (drawable != null) {
-                    bitmap = drawableToBitmap(drawable);
-                }
+                val drawable = packageManager.getApplicationIcon(
+                    /* p0 = */ packageName
+                )
+                bitmap = drawableToBitmap(drawable)
             }
-            return bitmap;
-        } catch (PackageManager.NameNotFoundException | Resources.NotFoundException e) {
-            e.printStackTrace();
-            return null;
+            bitmap
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            null
+        } catch (e: NotFoundException) {
+            e.printStackTrace()
+            null
         }
     }
 }
