2d00adc6bf0486f47ef82c6577d93d6d6dc7cdb5
loitp <www.muathu@gmail.com>
2023-03-20 11:50:58 +0700

    update

--- app/src/main/java/com/roy/enums/SortType.kt
+++ app/src/main/java/com/roy/enums/SortType.kt
@@ -1,8 +1,8 @@
-package com.roy.enums;
+package com.roy.enums
 
-import com.roy.R;
+import com.roy.R
 
-public enum SortType {
+enum class SortType(val displayNameResId: Int) {
     LABEL_ASCENDING(R.string.setting_sort_type_label_ascending),
     LABEL_DESCENDING(R.string.setting_sort_type_label_descending),
     INSTALL_DATE_ASCENDING(R.string.setting_sort_type_install_date_ascending),
@@ -12,13 +12,4 @@ public enum SortType {
     OPEN_COUNT_ASCENDING(R.string.setting_sort_type_open_count_ascending),
     OPEN_COUNT_DESCENDING(R.string.setting_sort_type_open_count_descending);
 
-    final int mDisplayNameResId;
-
-    SortType(int displayNameResId) {
-        mDisplayNameResId = displayNameResId;
-    }
-
-    public int getDisplayNameResId() {
-        return mDisplayNameResId;
-    }
 }
