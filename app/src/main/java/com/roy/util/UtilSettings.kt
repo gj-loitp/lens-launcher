c018711ee6924fd5634429550c18d5065118db0a
loitp <www.muathu@gmail.com>
2023-03-20 12:30:54 +0700

    update

--- app/src/main/java/com/roy/util/UtilSettings.kt
+++ app/src/main/java/com/roy/util/UtilSettings.kt
@@ -1,218 +1,254 @@
-package com.roy.util;
+package com.roy.util
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.NightMode
+import androidx.preference.PreferenceManager
+import com.roy.enums.SortType
 
-import androidx.appcompat.app.AppCompatDelegate;
+class UtilSettings(private val mContext: Context) {
+    companion object {
+        const val DEFAULT_ICON_SIZE = 18.0f
+        const val DEFAULT_DISTORTION_FACTOR = 2.5f
+        const val DEFAULT_SCALE_FACTOR = 1.0f
+        const val DEFAULT_ANIMATION_TIME: Long = 200
+        const val DEFAULT_VIBRATE_APP_HOVER = false
+        const val DEFAULT_VIBRATE_APP_LAUNCH = true
+        const val DEFAULT_SHOW_NAME_APP_HOVER = true
+        const val DEFAULT_SHOW_TOUCH_SELECTION = false
+        const val DEFAULT_SHOW_NEW_APP_TAG = true
+        const val DEFAULT_BACKGROUND = "Wallpaper"
+        const val DEFAULT_BACKGROUND_COLOR = "#FFF8BBD0"
+        const val DEFAULT_HIGHLIGHT_COLOR = "#FFF50057"
+        const val DEFAULT_ICON_PACK_LABEL_NAME = "Default Icon Pack"
+        const val DEFAULT_SORT_TYPE = 0
+        const val DEFAULT_NIGHT_MODE = AppCompatDelegate.MODE_NIGHT_NO
 
-import com.roy.enums.SortType;
-
-public class UtilSettings {
-
-    public static final float DEFAULT_ICON_SIZE = 18.0f;
-    public static final float DEFAULT_DISTORTION_FACTOR = 2.5f;
-    public static final float DEFAULT_SCALE_FACTOR = 1.0f;
-    public static final long DEFAULT_ANIMATION_TIME = 200;
-    public static final boolean DEFAULT_VIBRATE_APP_HOVER = false;
-    public static final boolean DEFAULT_VIBRATE_APP_LAUNCH = true;
-    public static final boolean DEFAULT_SHOW_NAME_APP_HOVER = true;
-    public static final boolean DEFAULT_SHOW_TOUCH_SELECTION = false;
-    public static final boolean DEFAULT_SHOW_NEW_APP_TAG = true;
-    public static final String DEFAULT_BACKGROUND = "Wallpaper";
-    public static final String DEFAULT_BACKGROUND_COLOR = "#FFF8BBD0";
-    public static final String DEFAULT_HIGHLIGHT_COLOR = "#FFF50057";
-    public static final String DEFAULT_ICON_PACK_LABEL_NAME = "Default Icon Pack";
-    public static final int DEFAULT_SORT_TYPE = 0;
-    public static final int DEFAULT_NIGHT_MODE = AppCompatDelegate.MODE_NIGHT_NO;
-
-    // These values are for the progress bars, their real values = (MAX_VALUE / INTERVAL (eg. 2)) + MIN_VALUE
-    public static final int MAX_ICON_SIZE = 45;
-    public static final int MAX_DISTORTION_FACTOR = 9;
-    public static final int MAX_SCALE_FACTOR = 5;
-    public static final int MAX_ANIMATION_TIME = 600;
-
-    public static final int SHOW_NEW_APP_TAG_DURATION = 12 * 60 * 60 * 1000; /* An app has the new tag for twelve hours. If openCount >= 1, the new tag is not drawn. */
-
-    public static final float MIN_ICON_SIZE = 10.0f;
-    public static final float MIN_DISTORTION_FACTOR = 0.5f;
-    public static final float MIN_SCALE_FACTOR = 1.0f;
-    public static final long MIN_ANIMATION_TIME = 100;
-
-    public static final float DEFAULT_FLOAT = Float.MIN_VALUE;
-    public static final long DEFAULT_LONG = Long.MIN_VALUE;
-    public static final boolean DEFAULT_BOOLEAN = false;
-    public static final String DEFAULT_STRING = "";
-
-    public static final String KEY_ICON_SIZE = "min_icon_size";
-    public static final String KEY_DISTORTION_FACTOR = "distortion_factor";
-    public static final String KEY_SCALE_FACTOR = "scale_factor";
-    public static final String KEY_ANIMATION_TIME = "animation_time";
-    public static final String KEY_VIBRATE_APP_HOVER = "vibrate_app_hover";
-    public static final String KEY_VIBRATE_APP_LAUNCH = "vibrate_app_launch";
-    public static final String KEY_SHOW_NAME_APP_HOVER = "show_name_app_hover";
-    public static final String KEY_SHOW_TOUCH_SELECTION = "show_touch_selection";
-    public static final String KEY_SHOW_NEW_APP_TAG = "show_new_tag_app";
-    public static final String KEY_BACKGROUND = "background";
-    public static final String KEY_BACKGROUND_COLOR = "background_color";
-    public static final String KEY_HIGHLIGHT_COLOR = "show_touch_selection_color";
-    public static final String KEY_ICON_PACK_LABEL_NAME = "icon_pack_label_name";
-    public static final String KEY_SORT_TYPE = "sort_type";
-    public static final String KEY_NIGHT_MODE = "night_mode";
-
-    private final Context mContext;
-    private SharedPreferences mPrefs;
-
-    public UtilSettings(Context context) {
-        mContext = context;
+        // These values are for the progress bars, their real values = (MAX_VALUE / INTERVAL (eg. 2)) + MIN_VALUE
+        const val MAX_ICON_SIZE = 45
+        const val MAX_DISTORTION_FACTOR = 9
+        const val MAX_SCALE_FACTOR = 5
+        const val MAX_ANIMATION_TIME = 600
+        const val SHOW_NEW_APP_TAG_DURATION =
+            12 * 60 * 60 * 1000 /* An app has the new tag for twelve hours. If openCount >= 1, the new tag is not drawn. */
+        const val MIN_ICON_SIZE = 10.0f
+        const val MIN_DISTORTION_FACTOR = 0.5f
+        const val MIN_SCALE_FACTOR = 1.0f
+        const val MIN_ANIMATION_TIME: Long = 100
+        const val DEFAULT_FLOAT = Float.MIN_VALUE
+        const val DEFAULT_LONG = Long.MIN_VALUE
+        const val DEFAULT_BOOLEAN = false
+        const val DEFAULT_STRING = ""
+        const val KEY_ICON_SIZE = "min_icon_size"
+        const val KEY_DISTORTION_FACTOR = "distortion_factor"
+        const val KEY_SCALE_FACTOR = "scale_factor"
+        const val KEY_ANIMATION_TIME = "animation_time"
+        const val KEY_VIBRATE_APP_HOVER = "vibrate_app_hover"
+        const val KEY_VIBRATE_APP_LAUNCH = "vibrate_app_launch"
+        const val KEY_SHOW_NAME_APP_HOVER = "show_name_app_hover"
+        const val KEY_SHOW_TOUCH_SELECTION = "show_touch_selection"
+        const val KEY_SHOW_NEW_APP_TAG = "show_new_tag_app"
+        const val KEY_BACKGROUND = "background"
+        const val KEY_BACKGROUND_COLOR = "background_color"
+        const val KEY_HIGHLIGHT_COLOR = "show_touch_selection_color"
+        const val KEY_ICON_PACK_LABEL_NAME = "icon_pack_label_name"
+        const val KEY_SORT_TYPE = "sort_type"
+        const val KEY_NIGHT_MODE = "night_mode"
     }
 
-    private SharedPreferences sharedPreferences() {
+    private var mPrefs: SharedPreferences? = null
+    private fun sharedPreferences(): SharedPreferences? {
         if (mPrefs == null) {
-            mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+            mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext)
         }
-        return mPrefs;
+        return mPrefs
     }
 
-    public void save(String name, int value) {
+    fun save(name: String?, value: Int) {
 //        sharedPreferences().edit().putInt(name, value).commit();
-        sharedPreferences().edit().putInt(name, value).apply();
+        sharedPreferences()!!.edit().putInt(name, value).apply()
     }
 
-    public @AppCompatDelegate.NightMode int getNightMode() {
-        switch (sharedPreferences().getInt(KEY_NIGHT_MODE, DEFAULT_NIGHT_MODE)) {
-            case AppCompatDelegate.MODE_NIGHT_AUTO:
-                return AppCompatDelegate.MODE_NIGHT_AUTO;
-            case AppCompatDelegate.MODE_NIGHT_NO:
-                return AppCompatDelegate.MODE_NIGHT_NO;
-            case AppCompatDelegate.MODE_NIGHT_YES:
-                return AppCompatDelegate.MODE_NIGHT_YES;
-            default:
-                return AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+    @get:NightMode
+    val nightMode: Int
+        get() = when (sharedPreferences()!!.getInt(
+            KEY_NIGHT_MODE,
+            DEFAULT_NIGHT_MODE
+        )) {
+            AppCompatDelegate.MODE_NIGHT_AUTO -> AppCompatDelegate.MODE_NIGHT_AUTO
+            AppCompatDelegate.MODE_NIGHT_NO -> AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.MODE_NIGHT_YES -> AppCompatDelegate.MODE_NIGHT_YES
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
         }
-    }
 
-    public void save(String name, float value) {
+    fun save(name: String?, value: Float) {
 //        sharedPreferences().edit().putFloat(name, value).commit();
-        sharedPreferences().edit().putFloat(name, value).apply();
+        sharedPreferences()!!.edit().putFloat(name, value).apply()
     }
 
-    public float getFloat(String name) {
-        switch (name) {
-            case KEY_ICON_SIZE:
-                if (sharedPreferences().getFloat(name, DEFAULT_ICON_SIZE) < MIN_ICON_SIZE) {
-                    save(name, MIN_ICON_SIZE);
-                } else if (sharedPreferences().getFloat(name, DEFAULT_ICON_SIZE) > getMaxFloatValue(name)) {
-                    save(name, getMaxFloatValue(name));
+    fun getFloat(name: String?): Float {
+        return when (name) {
+            KEY_ICON_SIZE -> {
+                if (sharedPreferences()!!.getFloat(
+                        name,
+                        DEFAULT_ICON_SIZE
+                    ) < MIN_ICON_SIZE
+                ) {
+                    save(name, MIN_ICON_SIZE)
+                } else if (sharedPreferences()!!.getFloat(
+                        name,
+                        DEFAULT_ICON_SIZE
+                    ) > getMaxFloatValue(name)
+                ) {
+                    save(name, getMaxFloatValue(name))
                 }
-                return sharedPreferences().getFloat(name, DEFAULT_ICON_SIZE);
-            case KEY_DISTORTION_FACTOR:
-                if (sharedPreferences().getFloat(name, DEFAULT_DISTORTION_FACTOR) < MIN_DISTORTION_FACTOR) {
-                    save(name, MIN_DISTORTION_FACTOR);
-                } else if (sharedPreferences().getFloat(name, DEFAULT_DISTORTION_FACTOR) > getMaxFloatValue(name)) {
-                    save(name, getMaxFloatValue(name));
-                }
-                return sharedPreferences().getFloat(name, DEFAULT_DISTORTION_FACTOR);
-            case KEY_SCALE_FACTOR:
-                if (sharedPreferences().getFloat(name, DEFAULT_SCALE_FACTOR) < MIN_SCALE_FACTOR) {
-                    save(name, MIN_SCALE_FACTOR);
-                } else if (sharedPreferences().getFloat(name, DEFAULT_SCALE_FACTOR) > getMaxFloatValue(name)) {
-                    save(name, getMaxFloatValue(name));
-                }
-                return sharedPreferences().getFloat(name, DEFAULT_SCALE_FACTOR);
-            default:
-                return sharedPreferences().getFloat(name, DEFAULT_FLOAT);
-        }
-    }
-
-    public void save(String name, long value) {
-//        sharedPreferences().edit().putLong(name, value).commit();
-        sharedPreferences().edit().putLong(name, value).apply();
-    }
-
-    public long getLong(String name) {
-        if (name.equals(KEY_ANIMATION_TIME)) {
-            if (sharedPreferences().getLong(name, DEFAULT_ANIMATION_TIME) < MIN_ANIMATION_TIME) {
-                save(name, MIN_ANIMATION_TIME);
-            } else if (sharedPreferences().getLong(name, DEFAULT_ANIMATION_TIME) > getMaxLongValue(name)) {
-                save(name, getMaxLongValue(name));
+                sharedPreferences()!!.getFloat(name, DEFAULT_ICON_SIZE)
             }
-            return sharedPreferences().getLong(name, DEFAULT_ANIMATION_TIME);
-        } else {
-            return sharedPreferences().getLong(name, DEFAULT_LONG);
+            KEY_DISTORTION_FACTOR -> {
+                if (sharedPreferences()!!.getFloat(
+                        name,
+                        DEFAULT_DISTORTION_FACTOR
+                    ) < MIN_DISTORTION_FACTOR
+                ) {
+                    save(name, MIN_DISTORTION_FACTOR)
+                } else if (sharedPreferences()!!.getFloat(
+                        name,
+                        DEFAULT_DISTORTION_FACTOR
+                    ) > getMaxFloatValue(name)
+                ) {
+                    save(name, getMaxFloatValue(name))
+                }
+                sharedPreferences()!!.getFloat(name, DEFAULT_DISTORTION_FACTOR)
+            }
+            KEY_SCALE_FACTOR -> {
+                if (sharedPreferences()!!.getFloat(
+                        name,
+                        DEFAULT_SCALE_FACTOR
+                    ) < MIN_SCALE_FACTOR
+                ) {
+                    save(name, MIN_SCALE_FACTOR)
+                } else if (sharedPreferences()!!.getFloat(
+                        name,
+                        DEFAULT_SCALE_FACTOR
+                    ) > getMaxFloatValue(name)
+                ) {
+                    save(name, getMaxFloatValue(name))
+                }
+                sharedPreferences()!!.getFloat(name, DEFAULT_SCALE_FACTOR)
+            }
+            else -> sharedPreferences()!!.getFloat(name, DEFAULT_FLOAT)
         }
     }
 
-    public void save(String name, String value) {
+    fun save(name: String?, value: Long) {
+//        sharedPreferences().edit().putLong(name, value).commit();
+        sharedPreferences()!!.edit().putLong(name, value).apply()
+    }
+
+    fun getLong(name: String): Long {
+        return if (name == KEY_ANIMATION_TIME) {
+            if (sharedPreferences()!!.getLong(
+                    name,
+                    DEFAULT_ANIMATION_TIME
+                ) < MIN_ANIMATION_TIME
+            ) {
+                save(name, MIN_ANIMATION_TIME)
+            } else if (sharedPreferences()!!.getLong(
+                    name,
+                    DEFAULT_ANIMATION_TIME
+                ) > getMaxLongValue(name)
+            ) {
+                save(name, getMaxLongValue(name))
+            }
+            sharedPreferences()!!.getLong(name, DEFAULT_ANIMATION_TIME)
+        } else {
+            sharedPreferences()!!.getLong(name, DEFAULT_LONG)
+        }
+    }
+
+    fun save(name: String?, value: String?) {
 //        sharedPreferences().edit().putString(name, value).commit();
-        sharedPreferences().edit().putString(name, value).apply();
+        sharedPreferences()!!.edit().putString(name, value).apply()
     }
 
-    public String getString(String name) {
-        switch (name) {
-            case KEY_BACKGROUND:
-                return sharedPreferences().getString(name, DEFAULT_BACKGROUND);
-            case KEY_BACKGROUND_COLOR:
-                return sharedPreferences().getString(name, DEFAULT_BACKGROUND_COLOR);
-            case KEY_HIGHLIGHT_COLOR:
-                return sharedPreferences().getString(name, DEFAULT_HIGHLIGHT_COLOR);
-            case KEY_ICON_PACK_LABEL_NAME:
-                return sharedPreferences().getString(name, DEFAULT_ICON_PACK_LABEL_NAME);
-            default:
-                return sharedPreferences().getString(name, DEFAULT_STRING);
+    fun getString(name: String?): String? {
+        return when (name) {
+            KEY_BACKGROUND -> sharedPreferences()!!.getString(
+                name,
+                DEFAULT_BACKGROUND
+            )
+            KEY_BACKGROUND_COLOR -> sharedPreferences()!!.getString(
+                name,
+                DEFAULT_BACKGROUND_COLOR
+            )
+            KEY_HIGHLIGHT_COLOR -> sharedPreferences()!!.getString(
+                name,
+                DEFAULT_HIGHLIGHT_COLOR
+            )
+            KEY_ICON_PACK_LABEL_NAME -> sharedPreferences()!!.getString(
+                name,
+                DEFAULT_ICON_PACK_LABEL_NAME
+            )
+            else -> sharedPreferences()!!.getString(name, DEFAULT_STRING)
         }
     }
 
-
-    public void save(String name, boolean value) {
+    fun save(name: String?, value: Boolean) {
 //        sharedPreferences().edit().putBoolean(name, value).commit();
-        sharedPreferences().edit().putBoolean(name, value).apply();
+        sharedPreferences()!!.edit().putBoolean(name, value).apply()
     }
 
-    public boolean getBoolean(String name) {
-        switch (name) {
-            case KEY_VIBRATE_APP_HOVER:
-                return sharedPreferences().getBoolean(name, DEFAULT_VIBRATE_APP_HOVER);
-            case KEY_VIBRATE_APP_LAUNCH:
-                return sharedPreferences().getBoolean(name, DEFAULT_VIBRATE_APP_LAUNCH);
-            case KEY_SHOW_NAME_APP_HOVER:
-                return sharedPreferences().getBoolean(name, DEFAULT_SHOW_NAME_APP_HOVER);
-            case KEY_SHOW_TOUCH_SELECTION:
-                return sharedPreferences().getBoolean(name, DEFAULT_SHOW_TOUCH_SELECTION);
-            case KEY_SHOW_NEW_APP_TAG:
-                return sharedPreferences().getBoolean(name, DEFAULT_SHOW_NEW_APP_TAG);
-            default:
-                return sharedPreferences().getBoolean(name, DEFAULT_BOOLEAN);
+    fun getBoolean(name: String?): Boolean {
+        return when (name) {
+            KEY_VIBRATE_APP_HOVER -> sharedPreferences()!!.getBoolean(
+                name,
+                DEFAULT_VIBRATE_APP_HOVER
+            )
+            KEY_VIBRATE_APP_LAUNCH -> sharedPreferences()!!.getBoolean(
+                name,
+                DEFAULT_VIBRATE_APP_LAUNCH
+            )
+            KEY_SHOW_NAME_APP_HOVER -> sharedPreferences()!!.getBoolean(
+                name,
+                DEFAULT_SHOW_NAME_APP_HOVER
+            )
+            KEY_SHOW_TOUCH_SELECTION -> sharedPreferences()!!.getBoolean(
+                name,
+                DEFAULT_SHOW_TOUCH_SELECTION
+            )
+            KEY_SHOW_NEW_APP_TAG -> sharedPreferences()!!.getBoolean(
+                name,
+                DEFAULT_SHOW_NEW_APP_TAG
+            )
+            else -> sharedPreferences()!!.getBoolean(name, DEFAULT_BOOLEAN)
         }
     }
 
-    public void save(SortType value) {
-        save(KEY_SORT_TYPE, value.ordinal());
+    fun save(value: SortType) {
+        save(KEY_SORT_TYPE, value.ordinal)
     }
 
-    public SortType getSortType() {
-        int ordinal = sharedPreferences().getInt(KEY_SORT_TYPE, DEFAULT_SORT_TYPE);
-        return SortType.values()[ordinal];
-    }
+    val sortType: SortType
+        get() {
+            val ordinal = sharedPreferences()!!.getInt(KEY_SORT_TYPE, DEFAULT_SORT_TYPE)
+            return SortType.values()[ordinal]
+        }
 
-    public float getMaxFloatValue(String name) {
-        switch (name) {
-            case KEY_ICON_SIZE:
-                return (float) MAX_ICON_SIZE + MIN_ICON_SIZE;
-            case KEY_DISTORTION_FACTOR:
-                return (float) MAX_DISTORTION_FACTOR / 2 + MIN_DISTORTION_FACTOR;
-            case KEY_SCALE_FACTOR:
-                return (float) MAX_SCALE_FACTOR / 2 + MIN_SCALE_FACTOR;
-            default:
-                return DEFAULT_FLOAT;
+    fun getMaxFloatValue(name: String?): Float {
+        return when (name) {
+            KEY_ICON_SIZE -> MAX_ICON_SIZE.toFloat() + MIN_ICON_SIZE
+            KEY_DISTORTION_FACTOR -> MAX_DISTORTION_FACTOR.toFloat() / 2 + MIN_DISTORTION_FACTOR
+            KEY_SCALE_FACTOR -> MAX_SCALE_FACTOR.toFloat() / 2 + MIN_SCALE_FACTOR
+            else -> DEFAULT_FLOAT
         }
     }
 
-    public long getMaxLongValue(String name) {
-        if (name.equals(KEY_ANIMATION_TIME)) {
-            return (long) MAX_ANIMATION_TIME / 2 + MIN_ANIMATION_TIME;
+    fun getMaxLongValue(name: String): Long {
+        return if (name == KEY_ANIMATION_TIME) {
+            MAX_ANIMATION_TIME.toLong() / 2 + MIN_ANIMATION_TIME
         } else {
-            return DEFAULT_LONG;
+            DEFAULT_LONG
         }
     }
 }
