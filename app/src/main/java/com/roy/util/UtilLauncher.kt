7232c9867b7e24da30019f47427a3700bf475350
loitp <www.muathu@gmail.com>
2023-03-20 12:18:48 +0700

    update

--- app/src/main/java/com/roy/util/UtilLauncher.kt
+++ app/src/main/java/com/roy/util/UtilLauncher.kt
@@ -1,47 +1,53 @@
-package com.roy.util;
+package com.roy.util
 
-import android.app.Application;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import com.roy.ui.AFakeLauncher
 
-import com.roy.ui.AFakeLauncher;
-
-public class UtilLauncher {
-
-    public static boolean isDefaultLauncher(Application application) {
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = application.getPackageManager().resolveActivity(intent, 0);
-        if (res.activityInfo == null) {
-            return false;
-        } else if ("android".equals(res.activityInfo.packageName)) {
-            return false;
+object UtilLauncher {
+    @JvmStatic
+    fun isDefaultLauncher(application: Application): Boolean {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        val res = application.packageManager.resolveActivity(intent, 0)
+        return if (res?.activityInfo == null) {
+            false
+        } else if ("android" == res.activityInfo.packageName) {
+            false
         } else {
-            return res.activityInfo.packageName.equals(application.getPackageName());
+            res.activityInfo.packageName == application.packageName
         }
     }
 
-    public static String getNameHomeLauncher(Application application) {
-        final Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        final ResolveInfo res = application.getPackageManager().resolveActivity(intent, 0);
-        if (res.activityInfo == null) {
-            return "";
-        }
-        return res.activityInfo.loadLabel(application.getPackageManager()).toString();
+    fun getNameHomeLauncher(application: Application): String {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        val res = application.packageManager.resolveActivity(intent, 0)
+        return if (res?.activityInfo == null) {
+            ""
+        } else res.activityInfo.loadLabel(application.packageManager).toString()
     }
 
-    public static void resetPreferredLauncherAndOpenChooser(Context context) {
-        PackageManager packageManager = context.getPackageManager();
-        ComponentName componentName = new ComponentName(context, AFakeLauncher.class);
-        packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-        Intent selector = new Intent(Intent.ACTION_MAIN);
-        selector.addCategory(Intent.CATEGORY_HOME);
-        selector.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(selector);
-        packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
+    @JvmStatic
+    fun resetPreferredLauncherAndOpenChooser(context: Context) {
+        val packageManager = context.packageManager
+        val componentName = ComponentName(context, AFakeLauncher::class.java)
+        packageManager.setComponentEnabledSetting(
+            /* p0 = */ componentName,
+            /* p1 = */ PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            /* p2 = */ PackageManager.DONT_KILL_APP
+        )
+        val selector = Intent(Intent.ACTION_MAIN)
+        selector.addCategory(Intent.CATEGORY_HOME)
+        selector.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context.startActivity(selector)
+        packageManager.setComponentEnabledSetting(
+            /* p0 = */ componentName,
+            /* p1 = */ PackageManager.COMPONENT_ENABLED_STATE_DEFAULT,
+            /* p2 = */ PackageManager.DONT_KILL_APP
+        )
     }
 }
